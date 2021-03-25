902106175f19e77263c1c417ba5aca839c1a27f8
Grzegorz00 <g.swiecickii@gmail.com>
2021-03-25 11:18:51 +0100

    count distance function

--- KNN.py
+++ KNN.py
@@ -22,14 +22,22 @@ class KNN:
         self.test_path = test_path
 
         # key - flower   value - algorithm result label
-        self.testList = fill_dictionary(test_path)
+        self.test_list = fill_dictionary(test_path)
         # key - flower   value - distance
         self.train_dictionary = fill_dictionary(train_path)
 
 
     def test(self):
-        iris = Iris([1, 5.1, 3.5, 1.4, 0.2], "Iris-setosa")
-        self.count_distance(iris)
+        # iris = Iris([1, 5.1, 3.5, 1.4, 0.2], "Iris-setosa")
+
+        for key, value in self.test_list.items():
+            self.count_distance(key)
+            self.test_list.update({key: self.find_best_label()})
+
+        # for key, value in self.test_list.items():
+        #     print(str(key.y) + " -> " + str(value))
+
+        print("--")
 
     def count_distance(self, iris):
         for train_iris in self.train_dictionary:
@@ -38,5 +46,15 @@ class KNN:
                 + pow(float(iris.x[1]) - float(train_iris.x[1]), 2)
                 + pow(float(iris.x[2]) - float(train_iris.x[2]), 2)
                 + pow(float(iris.x[3]) - float(train_iris.x[3]), 2)
+                + pow(float(iris.x[4]) - float(train_iris.x[4]), 2)
             )
             self.train_dictionary.update({train_iris: dist})
+
+    def find_best_label(self):
+        self.train_dictionary = {
+            k: v for k, v in sorted(self.train_dictionary.items(), key=lambda v: v[1])
+        }
+
+        return list(self.train_dictionary.values())[0]
+
+
