30d36b5c978dda4015f022594647c1a36181752b
Grzegorz00 <g.swiecickii@gmail.com>
2021-03-25 13:15:31 +0100

    while loop added

--- KNN.py
+++ KNN.py
@@ -25,19 +25,28 @@ class KNN:
         self.test_list = fill_dictionary(test_path)
         # key - flower   value - distance
         self.train_dictionary = fill_dictionary(train_path)
+        self.accuracy = 0
 
-
-    def test(self):
-        # iris = Iris([1, 5.1, 3.5, 1.4, 0.2], "Iris-setosa")
-
+    def test_file(self):
         for key, value in self.test_list.items():
             self.count_distance(key)
             self.test_list.update({key: self.find_best_label()})
 
-        # for key, value in self.test_list.items():
-        #     print(str(key.y) + " -> " + str(value))
+        print("Test value: -> Trained value:")
+        for key, value in self.test_list.items():
+            print(str(key.y) + " -> " + str(value))
+        print("Accuracy: " + str(self.count_accuracy()))
 
-        print("--")
+    def test_element(self, iris):
+        self.count_distance(iris)
+        print(str(iris.x) + " " + str(self.find_best_label()))
+
+    def count_accuracy(self):
+        correct = 0
+        for key, value in self.test_list.items():
+            correct = correct + 1 if str(key.y) == str(value) else correct
+
+        return str(correct / len(self.test_list))
 
     def count_distance(self, iris):
         for train_iris in self.train_dictionary:
@@ -55,6 +64,9 @@ class KNN:
             k: v for k, v in sorted(self.train_dictionary.items(), key=lambda v: v[1])
         }
 
-        return list(self.train_dictionary.values())[0]
+        k_elements = []
 
+        for i in range(0, int(self.k)):
+            k_elements.append(list(self.train_dictionary.keys())[i])
 
+        return max(set(k_elements), key=k_elements.count)
--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 from KNN import KNN
+from Iris import Iris
 
 k = input("Enter k value: ")
 # test_path = input("Enter test-set path name: ")
 # train_path = input("Enter train-set path name: ")
-
 test_path = "D:\\Studia\\PJATK\\Sem4\\NAI\\Ćwiczenia\\Lab2\\Projekt\\KNN-Iris\\dane\\test-set.csv"
 train_path = "D:\\Studia\\PJATK\\Sem4\\NAI\\Ćwiczenia\\Lab2\\Projekt\\KNN-Iris\\dane\\train-set.csv"
 
 knn = KNN(k, train_path, test_path)
-knn.test()
+knn.test_file()
 
+exit_program = False
 
+while not exit_program:
+    print("\n\n\nGive vector")
+    knn.test_element(Iris([input(), input(), input(), input(), input()], ""))
 
+    x = input("Press x to exit: ")
+    if x == 'x':
+        exit_program = True
 
-
-# exit = False
-#
-# while not exit:
-#
-#     x = input("Press x to exit")
-#     if x == 'x':
-#         exit = True
-#
-# print("End")
+print("End")
